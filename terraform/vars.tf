843b48e3e25648dea17269598b696e5cd3541737
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-03-04 17:17:53 +0100

    Add files via upload

---
+++ terraform/vars.tf
@@ -0,0 +1,16 @@
+﻿variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "West Europepacker build demo.json"
+}
+
+variable "username" {
+    description = "Admin"
+}
+
+variable "password"{
+  description = "Admin password"
+}
